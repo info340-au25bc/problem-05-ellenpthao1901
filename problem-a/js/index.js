244474b17e9bf2f97581d25873c1bf0e07a78252
ellenpthao1901 <thao19@uw.edu>
2025-10-22 20:22:36 -0700

    problem a answer

--- problem-a/js/index.js
+++ problem-a/js/index.js
@@ -3,32 +3,37 @@
 /** Basic Types */
 
 //Define a new variable `motto` for the value "The iSchool is my school"
-
+let motto = "The iSchool is my school";
 
 //Log out the motto
-
+console.log(motto);
 
 //Define a variable `mottoLength` that stores the length of the motto.
 //Use the `.length` property
 //Log out the length.
-
+let mottoLength = motto.length;
+console.log(mottoLength);
 
 //Use the `indexOf()` String method to see if the word "cool" is in the string.
 //See http://www.w3schools.com/jsref/jsref_obj_string.asp for String methods
 //Log out a _boolean_ whether it is or not.
-
+let result = motto.indexOf("cool") !== -1;
+console.log(result);
 
 //Use a String method to replace the word "iSchool" in the `motto` variable with
 //the words "Information School". The new value should be re-assigned to the
 //`motto` variable.
 //Log out the updated motto.
-
+motto =motto.replaceAll("iSchool", "Information School");
+console.log(motto);
 
 //Calculate the ratio between the length of the updated motto and the length of 
 //the old (which you had saved in a variable!). Log out this ratio as a 
 //percentage with two decimal places of precision (e.g., `"123.45%"`).
 //You can use the `.toFixed()` Number method to specify the precision. 
-
+let mottoLengthNew = motto.length;
+let ratio = (mottoLengthNew/ mottoLength) * 100;
+console.log(ratio.toFixed(2)+"%");
 
 
 /** Arrays **/
@@ -36,20 +41,35 @@
 //Create an array `numbers` that contains these 10 numbers:
 //    1 4 1 5 9 2 6 5 3 5
 //Log out the array.
-
+let numbers = [1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+console.log(numbers);
 
 //Use bracket notation to change the `4` in the array to a `4.2`.
 //Log out the updated array.
-
+let thatIndex = numbers.indexOf(4);
+numbers[thatIndex] = 4.2;
+console.log(numbers);
 
 //Add the number 3 to the END of the array.
 //Log out the updated array.
-
+numbers.push(3);
+console.log(numbers);
 
 //Find the median (middle) value of the numbers in the array.
 //Hint: sort() the array, then access the middle index of the sorted values.
 //You can use the `Math.floor()` function to round to a whole number.
 //Log out the median value.
+numbers.sort();
+if (numbers.length%2==0) {
+    let indexEven1 = numbers.length/2;
+    let indexEven2 = numbers.length/2 - 1;
+    let medianEven = Math.floor((numbers[indexEven1] + numbers[indexEven2])/2);
+    console.log(medianEven);
+} else {
+    let indexOdd = Math.floor(numbers.length / 2);
+    let medianOdd = numbers[indexOdd];
+    console.log(medianOdd);
+}
 
 
 
@@ -59,19 +79,21 @@
 //with properties:
 //  `x` (coordinate) of 30, `y` of 50, `width` of 100, `height` of 50
 //Log out the rectangle object
-
+let rect = {x:30, y:50, width: 100, height:50};
+console.log(rect);
 
 //Log out the x- and y- coordinates of the rectangle (its location). Your output 
 //should have the format `"X, Y"`.
-
+console.log(rect.x + ", " + rect.y);
 
 //Set the rectangle's height to be the square root of its width. (Use the 
 //`Math.sqrt()` function).
 //Use *dot notation* to access the properties!
-
+rect.height = Math.sqrt(rect.width);
 
 //Log out the rectangle's area. Use *dot notation* to access the properties!
-
+let rectArea = rect.height * rect.width;
+console.log(rectArea);
 
 //Create a variable `circle` that represents a circle. This should be an object
 //with properties:
@@ -79,25 +101,35 @@
 //  `cy` of 43,
 //  `radius` equal to the LAST value in the (sorted) `numbers` array.
 //Log out the circle
-
+let lengthNum = numbers.length;
+let lastNum = numbers[lengthNum - 1];
+let circle = {
+    cx: 34,
+    cy: 43,
+    radius: lastNum
+};
+console.log(circle);
 
 //Create an array `shapes` that represents a list of shapes. The array should
 //contain the rectangle and the circle objects defined above.
 //Log out the variable. Be sure to inspect it in the developer console!
-
+let shapes = [rect, circle];
+console.log(shapes);
 
 //Add a new ANONYMOUS object (e.g., one passed in directly without its own
 //variable name) representing a right triangle to the `shapes` array.
 //The triangle should have a `base` of 33 and a `height` of 44.
 //Log out the updated shapes array.
-
+shapes.push({base: 33, height: 44});
+console.log(shapes);
 
 //Log out the triangle's `hypotenuse` property (don't calculate it, just log out
 //the current property value!). What do you get?
-
+console.log(shapes[2].hypotenuse);
 
 //Assign the triangle inside the `shapes` array a 'hypotenuse' property of `55`.
 //Log out the `shapes` array again.
 //Visually check: what happens if you inspect the previously logged array in the 
 //Chrome developer console?
-
+shapes[2].hypotenuse = 55;
+console.log(shapes);
